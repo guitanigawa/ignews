0f1dfa4408f28449b1a2dbc16a32b73b6abc9d15
guitanigawa <guitanigawa07@gmail.com>
2022-12-08 01:07:07 -0300

    a

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head"
+import styles from "./styles.module.css"
+
+export default function Posts(){
+    return(
+        <>
+            <Head>
+                <title>Posts - ig.news</title>
+            </Head>
+
+            <main>
+                <div>
+                    <a>
+                        <time></time>
+                        <strong></strong>
+                        <p></p>
+                    </a>
+                </div>
+            </main>
+        </>
+    )
+}
